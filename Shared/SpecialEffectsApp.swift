6a94eb2daa468a43915b1890ab21718a1cf7f3fd
Justin Purnell <jpurnell@users.noreply.github.com>
2022-02-25 14:11:40 -0500

    Initial Commit

---
+++ Shared/SpecialEffectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpecialEffectsApp.swift
+//  Shared
+//
+//  Created by Justin Purnell on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SpecialEffectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
